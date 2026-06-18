f6dd8a196d75db78f72cbae6ff1e434b61fc4e3e
Augusto Pereira <costapereira620@gmail.com>
2026-06-17 22:05:31 -0300

    Update run_mozbc.sh

--- run_mozbc.sh
+++ run_mozbc.sh
@@ -6,10 +6,11 @@
 # module load hdf5/1.10.8
 # module load netcdf/4.9.2
 
-module purge
 module load gnu9/9.4.0
-module avail hdf5
-module avail netcdf
+module load mpich/3.4.2-ucx
+module load phdf5/1.10.8
+module load netcdf/4.7.4
+module load netcdf-fortran/4.5.3
 
 cp /home/augusto.pereira/BUILD_WRF/WRF-4.6.0/1MY_SUMULATION/data_mozbc/mozbc.inp-FOR-GOCART301 /home/augusto.pereira/BUILD_WRF/Processors/mozbc/mozbc.inp
 
